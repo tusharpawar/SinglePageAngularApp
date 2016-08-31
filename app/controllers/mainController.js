239ab8a5dbeeac9b16466af4b58f63849c72117b
tusharpawar <tusharpawar2012@gmail.com>
2016-08-31 12:17:19 -0700

    some final changes

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -4,14 +4,12 @@
 
 			  $scope.users = [];
 
-			  $scope.usersToAdd = [{
-			    name: ''
-			  }];
+			  $scope.usersToAdd = [];
 
 			  $scope.add = function(userToAdd) {
 
 			    var index = $scope.usersToAdd.indexOf(userToAdd);
-
+			    
 			    $scope.usersToAdd.splice(index, 1);
 
 			    $scope.users.push(angular.copy(userToAdd))
